2d47892ce28e278225dff75417a89a0f797da7de
HipolitoRosado <paulserrano58@gmail.com>
2020-03-03 13:13:28 -0600

    Add project files.

---
+++ Lesson6_2_Paul/Animal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_2_Paul
+{
+    class Animal
+
+    {
+        private int legs { get; set; }
+        private int arms { get; set; }
+        private string type { get; set; }
+        private string name { get; set; }
+        private bool isDangerous { get; set; }
+
+        public Animal(int legs, int arms, string type, string name, bool isDanerous)
+        {
+            this.legs = legs;
+            this.arms = arms;
+            this.name = name;
+            this.isDangerous = isDangerous;
+
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Type of animal: " + type);
+            Console.WriteLine("Number of arms: " + arms);
+            Console.WriteLine("Number of legs: " + legs);
+            Console.WriteLine("Dangerous?: " + isDangerous);
+
+            }
+        
+        public void SendToObedienceSchool()
+        {
+            if (isDangerous)
+            {
+                isDangerous = false;
+                Console.WriteLine(name + " is a good animal" + type + "now");
+            }
+            else
+            {
+                Console.WriteLine(name + " is'nt dangerous");
+            }
+        }
+
+        public void Evolve(int newArms, int newLegs)
+        {
+            arms = newArms;
+            legs = newLegs;
+            type = "Something Else!";
+        }
+
+
+    }
+}
---
+++ Lesson6_2_Paul/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Lesson6_2_Paul
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal(4, 0, "Cat", "Snowball", true);
+            Animal animal2 = new Animal(8, 0, "Spider", "Friendly", false);
+            Animal animal3 = new Animal(2, 2, "Monkey", "Bobo", false);
+
+
+            animal1.ShowDetails();
+            animal2.ShowDetails();
+            animal3.ShowDetails();
+
+
+            animal1.SendToObedienceSchool();
+            animal2.SendToObedienceSchool();
+            animal3.SendToObedienceSchool();
+
+            animal1.Evolve(2,12);
+            animal1.ShowDetails();
+        }
+
+    }
+}
+    
+
